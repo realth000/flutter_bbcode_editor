0c33eccf9219b50e87594508b7aa81aab9490eff
realth000 <a598919335@gmail.com>
2025-05-29 21:11:38 +0800

    revert: plain spoiler and hide again

--- lib/src/tags/hide/hide_buttton.dart
+++ lib/src/tags/hide/hide_buttton.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bbcode_editor/flutter_bbcode_editor.dart';
 import 'package:flutter_bbcode_editor/src/editor.dart';
@@ -21,10 +23,10 @@ class BBCodeEditorToolbarHideButton extends StatelessWidget {
       iconTheme: const QuillIconTheme(),
       isSelected: false,
       onPressed: () async {
-        // if (Platform.isAndroid) {
-        //   controller.insertRawCode('[hide]', '[/hide]');
-        //   return;
-        // }
+        if (Platform.isAndroid) {
+          controller.insertRawCode('[hide]', '[/hide]');
+          return;
+        }
         controller.insertEmbeddable(BBCodeHideEmbed(BBCodeHideInfo.buildEmpty()));
       },
     );
